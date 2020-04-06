af6f59564bdd3a5e1a454820b98a8e0e409abc62
hahava <hahava5696@gmail.com>
2020-04-06 21:11:55 +0900

    Add daily dev log

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,12 +2,24 @@ var store = [{
         "title": "2020.03.30-개발일지",
         "excerpt":"주제 Scanner 또는 System.out.println 을 유틸 함수로 래핑하여 중복을 제거 목표 유틸클래스를 작성하여 해당 메서드에서 유효성검사 등의 기능을 제공 고민한 내용 BufferdReader close() 호출 후 다시 객체를 생성하려 하면 java.io.IOException: Stream closed 에러 발생 알게된 내용 System.in System 클래스의 in 인스턴스의 설명은 아래와 같이 주석으로 잘 되어 있음 /** *...","categories": ["dev_log"],
         "tags": ["dev_log","java"],
-        "url": "http://localhost:4000/dev_log/daily-dev-log/",
+        "url": "http://localhost:4000/dev_log/daily-dev-log20200330/",
         "teaser": null
       },{
         "title": "kotlin study day.1",
-        "excerpt":"Kotiln study Functions Entry Poin kotlin 애플리케이션은 main 함수에서 시작합니다. fun main(){ println(\"hello world\") } Funtion kotiln 에서 함수 선언은 fun 키워드를 사용합니다. fun double(x: Int): Int{ return 2 * x } val result = double(2) parameter 함수의 매개변수는 와 반환 값은 : 를 이용하여 타입을 지정합니다. 만약 반환값으로 :...","categories": ["study"],
+        "excerpt":"Functions Entry Poin kotlin 애플리케이션은 main 함수에서 시작합니다. fun main(){ println(\"hello world\") } Funtion kotiln 에서 함수 선언은 fun 키워드를 사용합니다. fun double(x: Int): Int{ return 2 * x } val result = double(2) parameter 함수의 매개변수는 와 반환 값은 : 를 이용하여 타입을 지정합니다. 만약 반환값으로 : 를 지정하지...","categories": ["study"],
         "tags": ["kotlin"],
         "url": "http://localhost:4000/study/kotlin-day1/",
+        "teaser": null
+      },{
+        "title": "kotlin study day.2",
+        "excerpt":"Variables kotlin은 java의 멤버 변수를 properties로 명명합니다. properties는 멤버변수와 비슷하게 작동하지만 일부의 차이점이 있습니다. 특징은 아래와 같습니다. 타입추론 var a = \"1234\" // 타입을 지정하지 않아도 String으로 작동 // var a; -&gt; error var b: String = \"12345\" var c: String c = \"12345\" 변수 선언 및 할당시 kotlin에서는 타입...","categories": ["study"],
+        "tags": ["kotlin"],
+        "url": "http://localhost:4000/study/kotlin-day2/",
+        "teaser": null
+      },{
+        "title": "2020.04.06-개발일지",
+        "excerpt":"주제 어떻게 하면 switch 지옥에서 벗어날 수 있을까? 이게 뭐하는 짓이람 Commnad Line user Interface 환경에서 View 와 사용자 input를 어떻게 해야 연관성있게 가져갈 수 있을까? 이렇게 하면 메뉴를 수정할 때 마다 input 관련해서 전부 수정해야 한다. 목표 코드를 깔끔하게… 수정하기 쉽게 …. 고민한 내용 디자인 패턴을 이용하여 개발 기본적으로...","categories": ["dev_log"],
+        "tags": ["dev_log","java"],
+        "url": "http://localhost:4000/dev_log/daily-dev-log20200406/",
         "teaser": null
       }]
